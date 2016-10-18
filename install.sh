ebf5a8c5268ce052b5dfd9fa31895c0ec2a04290
Hendrik Mans <hendrik@mans.de>
2016-10-18 09:03:46 +0200

    Emoji power!

--- install.sh
+++ install.sh
@@ -12,8 +12,10 @@ install () {
   DEST="$HOME/$1"
 
   if [ -h "$DEST" ]; then      # if $DEST is a symlink, we can probably delete it.
+    echo "❌  Removing symlink $DEST"
     run rm "$DEST"
   elif [ -e "$DEST" ]; then
+    echo "💾  Backing up $DEST"
     run mv "$DEST" "$DEST.orig-$NOW"
   fi
   run ln -s "$SRC" "$DEST"
