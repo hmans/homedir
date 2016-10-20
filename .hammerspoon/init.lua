52b4fbaa36f63942b5abac67b9432dba4983c0b5
Hendrik Mans <hendrik@mans.de>
2016-10-20 11:52:59 +0200

    [Hammerspoon] Screen corners tweaks

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -50,11 +50,11 @@ bindmovetoratio("Right", 0.5, 0,   0.5, 1)
 bindmovetoratio("Up",    0,   0,   1,   0.5)
 bindmovetoratio("Down",  0,   0.5, 1,   0.5)
 
--- Screen corners
-bindmovetoratio("1",     0,   0,   0.7, 0.7)
-bindmovetoratio("2",     0.3, 0,   0.7, 0.7)
-bindmovetoratio("3",     0.3, 0.3, 0.7, 0.7)
-bindmovetoratio("4",     0,   0.3, 0.7, 0.7)
+-- Screen corners, with slight overlap
+bindmovetoratio("1",     0,   0,   0.6, 0.6)
+bindmovetoratio("2",     0.4, 0,   0.6, 0.6)
+bindmovetoratio("3",     0.4, 0.4, 0.6, 0.6)
+bindmovetoratio("4",     0,   0.4, 0.6, 0.6)
 
 
 ---
