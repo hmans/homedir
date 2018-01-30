24ca5b7377990ede51ac0db0b1ef7b9507c42fa9
Hendrik Mans <hendrik@mans.de>
2018-01-30 16:40:24 +0100

    Reconfigure Postgres.app for Fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ source /usr/local/share/chruby/auto.fish
 # Custom stuff
 #set PATH $PATH /usr/local/mysql/bin
 
+# Postgres.app
+set PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
