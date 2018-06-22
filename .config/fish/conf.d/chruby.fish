c8a85a57528dedb47fd90fce8c02886a8a05d6e5
Hendrik Mans <hendrik@mans.de>
2018-06-22 18:55:13 +0000

    Make chruby.fish safer

--- .config/fish/conf.d/chruby.fish
+++ .config/fish/conf.d/chruby.fish
@@ -1,3 +1,5 @@
 # chruby-fish
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+if test -d /usr/local/share/chruby
+  source /usr/local/share/chruby/chruby.fish
+  source /usr/local/share/chruby/auto.fish
+end
