3670f71273f7ea713ca5d5f7ecfbd7b9264a3155
Hendrik Mans <hendrik@mans.de>
2019-02-19 11:55:03 +0100

    Add kubectl aliases for XING

--- .config/fish/conf.d/xing.fish
+++ .config/fish/conf.d/xing.fish
@@ -1,3 +1,8 @@
 if test -d /Users/hmans/.oly/bin
   set PATH /Users/hmans/.oly/bin $PATH
 end
+
+alias kubectl-main-production "kubectl --context kubernetes.main.production.ams1"
+alias kubectl-misc-production "kubectl --context kubernetes.misc.production.ams1"
+alias kubectl-main-preview "kubectl --context kubernetes.main.preview.ams1"
+alias kubectl-misc-preview "kubectl --context kubernetes.misc.preview.ams1"
