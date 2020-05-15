b02f1b3491886ecbd0ea9a5fb3fdb1a1c48c02dd
Hendrik Mans <hendrik@mans.de>
2020-05-15 16:28:08 +0200

    chruby-fish in WSL2

--- .config/fish/conf.d/chruby.fish
+++ .config/fish/conf.d/chruby.fish
@@ -3,3 +3,10 @@ if test -d /usr/local/share/chruby
   source /usr/local/share/chruby/chruby.fish
   source /usr/local/share/chruby/auto.fish
 end
+
+# linuxbrew
+if test -d /home/linuxbrew/.linuxbrew/opt/chruby
+  source /home/linuxbrew/.linuxbrew/opt/chruby-fish/share/chruby/chruby.fish
+  source /home/linuxbrew/.linuxbrew/opt/chruby-fish/share/chruby/auto.fish
+  set -g -x CHRUBY_ROOT /home/linuxbrew/.linuxbrew/
+end
