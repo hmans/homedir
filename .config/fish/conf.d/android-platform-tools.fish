9928928b0863b8f48175a0d7c162dd79dbc0a588
Hendrik Mans <hendrik@mans.de>
2020-06-18 18:39:38 +0200

    Additional paths for android SDK

--- .config/fish/conf.d/android-platform-tools.fish
+++ .config/fish/conf.d/android-platform-tools.fish
@@ -1,1 +1,7 @@
-set PATH $PATH $HOME/src/android-platform-tools
+if test -d $HOME/Android/Sdk/platform-tools
+    set PATH $PATH $HOME/Android/Sdk/platform-tools
+end
+
+if test -d $HOME/src/android-platform-tools
+    set PATH $PATH $HOME/src/android-platform-tools
+end
